baf4b5be5f16291c44a7ecaeb38e0c3f3210e2b5
Viktor Mörsell <viktor.morsell@northvolt.com>
2020-10-12 03:38:22 +0200

    Set unit based on rounded values

--- utils/date.ts
+++ utils/date.ts
@@ -49,14 +49,14 @@ export const timeTo = ({ dateTime }: TimeToProps): string => {
 
   if (Math.abs(weeks) > 0) {
     const rounded = Math.round(weeks)
-    const unit = weeks > 1 ? 'weeks' : 'week'
+    const unit = rounded > 1 ? 'weeks' : 'week'
     return `${rounded} ${unit}${ago}`
   }
 
   const days = weeks * 7
   if (Math.abs(days) > 0) {
     const rounded = Math.round(days)
-    const unit = days > 1 ? 'days' : 'day'
+    const unit = rounded > 1 ? 'days' : 'day'
     return `${rounded} ${unit}${ago}`
   }
 
